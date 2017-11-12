33f673cfa84678d0c0c16f0dd667811d1d67225c
Antartakit <33598304+Antartakit@users.noreply.github.com>
2017-11-12 14:05:24 +0000

    Add files via upload

---
+++ $tagsaV1.0.0.js
@@ -0,0 +1,152 @@
+var $tags = {
+	//Text Formating
+	body: {
+		o: ["<body>"],
+		c: ["</body>"],
+		u: ["<body "],
+	},
+	b: {
+		o: ["<b>"],
+		c: ["</b>"],
+		u: ["<b "],
+	},
+	strong: {
+		o: ["<strong>"],
+		c: ["</strong>"],
+		u: ["<strong "],
+	},
+	i: {
+		o: ["<i>"],
+		c: ["</i>"],
+		u: ["<i "],
+	},
+	sub: {
+		o: ["<sub>"],
+		c: ["</sub>"],
+		u: ["<sub "],
+	},
+	sup: {
+		o: ["<sup>"],
+		c: ["</sup>"],
+		u: ["<sup "],
+	},
+	em: {
+		o: ["<em>"],
+		c: ["</em>"],
+		u: ["<em "],
+	},
+	mark: {
+		o: ["<mark>"],
+		c: ["</mark>"],
+		u: ["<mark "],
+	},
+	small: {
+		o: ["<small>"],
+		c: ["</small>"],
+		u: ["<small "],
+	},
+	deleted: {
+		o: ["<deleted>"],
+		c: ["</deleted>"],
+		u: ["<deleted "],
+	},
+	ins: {
+		o: ["<ins>"],
+		c: ["</ins>"],
+		u: ["<ins "],
+	},
+	u: {
+		o: ["<u>"],
+		c: ["</u>"],
+		u: ["<u "],
+	},
+	
+	//Text Styling
+	div: {
+		o: ["<div>"],
+		c: ["</div>"],
+		u: ["<div "],
+	},
+	title: {
+		o: ["<title>"],
+		c: ["</title>"],
+		u: ["<title "],
+	},
+	p: {
+		o: ["<p>"],
+		c: ["</p>"],
+		u: ["<p "],
+	},
+	h1: {
+		o: ["<h1>"],
+		c: ["</h1>"],
+		u: ["<h1 "],
+	},
+	h2: {
+		o: ["<h2>"],
+		c: ["</h2>"],
+		u: ["<h2 "],
+	},
+	h3: {
+		o: ["<h3>"],
+		c: ["</h3>"],
+		u: ["<h3 "],
+	},
+	h4: {
+		o: ["<h4>"],
+		c: ["</h4>"],
+		u: ["<h4 "],
+	},
+	h5: {
+		o: ["<h5>"],
+		c: ["</h5>"],
+		u: ["<h5 "],
+	},
+	h6: {
+		o: ["<h6>"],
+		c: ["</h6>"],
+		u: ["<h6 "],
+	},
+	footer: {
+		o: ["<footer>"],
+		c: ["</footer>"],
+		u: ["<footer "],
+	},
+	li: {
+		o: ["<li>"],
+		c: ["</li>"],
+		u: ["<li "],
+	},
+	hr: {
+		o: ["<hr>"],
+		c: ["</hr>"],
+		u: ["<hr "],
+	},
+	ol: {
+		o: ["<ol>"],
+		c: ["</ol>"],
+		u: ["<ol "],
+	},
+	ul: {
+		o: ["<ul>"],
+		c: ["</ul>"],
+		u: ["<ul "],
+	},
+	a: {
+		o: ["<a>"],
+		c: ["</a>"],
+		u: ["<a "],
+	},
+	
+	//Other Code
+	style: {
+		o: ["<style>"],
+		c: ["</style>"],
+		u: ["<style "],
+	},
+	ruby: {
+		o: ["<ruby>"],
+		c: ["</ruby>"],
+		u: ["<ruby "],
+	},
+};
